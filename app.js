7a56fe3b27aa1af19c10b01906a510037112a84b
Haris <haris.markcoders@gmail.com>
2024-06-05 00:22:41 +0500

    ... minor changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.routes.js";
 import adminRouter from "./routes/admin.routes.js"
 import morgan from "morgan";
-import {limiterDefault, limiterCheck, limiterBreak, limiterPassword} from "./middleware/limit.middleware.js";
+import { defaultLimiter, checkLimiter, breakLimiter, passwordLimiter} from "./middleware/limit.middleware.js";
 const app = express();
 
 app.use(
@@ -23,11 +23,11 @@ app.use(morgan("tiny"))
 // checkin checkout 1 hour me 1 only
 // reset-password and change-password once per day
 
-app.use(limiterDefault)
-app.use('/api/check' , limiterCheck )
-app.use('/api/break', limiterBreak)
-app.use('/api/reset-password' , limiterPassword )
-app.use('/api/change-password' , limiterPassword )
+app.use(defaultLimiter)
+app.use('/api/check' , checkLimiter )
+app.use('/api/break', breakLimiter)
+app.use('/api/reset-password' , passwordLimiter  )
+app.use('/api/change-password' , passwordLimiter )
 
 app.use(
   express.json({
--- middleware/limit.middleware.js
+++ middleware/limit.middleware.js
@@ -3,7 +3,7 @@
 import { rateLimit } from 'express-rate-limit';
 
 
-export const limiterDefault = rateLimit({
+export const defaultLimiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 1 minute
 	limit: 20 , // Limit each IP to 100 requests per `window` (here, per 20 minutes).
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
@@ -15,7 +15,7 @@ export const limiterDefault = rateLimit({
 });
 
 
-export const limiterBreak = rateLimit({
+export const breakLimiter = rateLimit({
 	windowMs: 5 * 60 * 1000, // 5 minutes
 	limit: 1 , 
 	
@@ -23,7 +23,7 @@ export const limiterBreak = rateLimit({
         return {message: "You can only break once per 5 Minutes"}}
 });
 
-export const limiterCheck = rateLimit({
+export const checkLimiter = rateLimit({
 	windowMs: 60 * 60 * 1000, // 60 minutes
 	limit: 1 , 
 
@@ -31,7 +31,7 @@ export const limiterCheck = rateLimit({
         return {message: "You can only check once per Hour"}}
 });
 
-export const limiterPassword = rateLimit({
+export const passwordLimiter = rateLimit({
     windowMs: 24*60*60*1000,  // 1 day
     limit: 1,
     message: ()=>{
