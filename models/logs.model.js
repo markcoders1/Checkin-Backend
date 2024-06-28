8ee07475347349974927a42f4c5ed54010fe0ea0
Shahzaibalikhawaja <shahzaib.ali.khawaja@gmail.com>
2024-06-29 00:30:13 +0500

    Added limit 10 to logs for each user

--- models/logs.model.js
+++ models/logs.model.js
@@ -27,24 +27,41 @@ const logSchema = new mongoose.Schema({
 	{ timestamps: true }
 )
 
-logSchema.methods.logger = async function(userId, deviceIds, logType) {
-    try {
-        // Ensure deviceIds is always treated as an array
-        const ids = Array.isArray(deviceIds) ? deviceIds : [deviceIds];
+logSchema.methods.logger = async function (userId, deviceIds, logType) {
+	try {
+		// Ensure deviceIds is always treated as an array
+		const ids = Array.isArray(deviceIds) ? deviceIds : [deviceIds];
 
-        // Iterate over each deviceId and create a log
-        for (const id of ids) {
-            await this.model('Log').create({
-                userId: userId,
-                deviceId: id,
-                logType: logType
-            });
-            console.log(`"${logType}" log created for deviceId: ${id}`);
-        }
-    } catch (error) {
-        console.error(`Error creating ${logType} logs:`, error);
-        throw error;
-    }
+		// Get current count of logs for the user
+		const logCount = await this.model("Log").countDocuments({ userId });
+
+		// If log count exceeds 100, delete the oldest logs for this user
+		if (logCount >= 10) {
+			const oldestLogs = await this.model("Log").find(
+				{ userId },
+				{},
+				{ sort: { createdAt: 1 }, limit: logCount - 9 }
+			);
+			for (const log of oldestLogs) {
+				await log.remove();
+                await log.save()
+				console.log(`Oldest log removed for userId ${userId}`);
+			}
+		}
+
+		// Iterate over each deviceId and create a log
+		for (const id of ids) {
+			await this.model("Log").create({
+				userId: userId,
+				deviceId: id,
+				logType: logType,
+			});
+			console.log(`"${logType}" log created for deviceId: ${id}`);
+		}
+	} catch (error) {
+		console.error(`Error creating ${logType} logs:`, error);
+		throw error;
+	}
 };
 
 export const Log = mongoose.model("Log", logSchema);
